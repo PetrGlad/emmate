5f439ef8bfadf44c8ce49be8252233325d44b3cf
Petr Gladkikh <petrglad@gmail.com>
2022-11-03 23:22:40 +0100

    See if non-default output device can be used
    Trying to reduce midi-event->sound delays.
    
    TODO To reduce delays try to pick other devices for output
    (pick a device from cpal and make a rodio stream from it).
    Experimented with sine wave source and there are similar delays after
    the midi event as with a plugin-generated sound. So delayz are likely
    at the output (buffering or the device).

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,9 @@ use midly::{TrackEvent, TrackEventKind};
 use midly::io::Cursor;
 use midly::MidiMessage::NoteOn;
 use midly::TrackEventKind::Midi;
-use rodio::OutputStream;
+use rodio::{cpal, OutputStream, Source};
 use rodio::source::SineWave;
+use rodio::source::TakeDuration;
 use vst::api::Events;
 use vst::event::Event;
 use vst::host::{Host, HostBuffer};
@@ -40,7 +41,10 @@ pub fn main() {
         // use log::*;
         // stderrlog::new()/*.module(module_path!())*/.verbosity(Level::Trace).init().unwrap();
     }
-    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+
+    list_all_devicess();
+
+    let (_stream, stream_handle) = rodio::OutputStream::try_from_device(cpal::()).unwrap();
     {
         let vst = Vst::init();
         {
@@ -50,10 +54,13 @@ pub fn main() {
             // beep_sink.set_volume(0.3);
 
             // Example: Sound from a generative source:
-            // stream_handle.play_raw(SineWave::new(1000.0)).unwrap();
+            // stream_handle.play_raw(
+            //     SineWave::new(1000.0)
+            //         .take_duration(Duration::from_millis(200)))
+            //     .unwrap();
 
             // Sound from the VST host:
-            stream_handle.play_raw(OutputSource::new(&vst, 1 << 10)).unwrap();
+            // stream_handle.play_raw(OutputSource::new(&vst, 128)).unwrap();
         }
 
         // {
@@ -77,7 +84,8 @@ pub fn main() {
             for (i, port) in ports.iter().enumerate() {
                 let name = input.port_name(&port).unwrap();
                 println!("\t{}", name);
-                if name.starts_with("Digital Piano") {
+                // if name.starts_with("Digital Piano") {
+                if name.starts_with("MPK mini 3") {
                     port_idx = Some(i);
                     println!("Selected MIDI input: '{}'", name);
                     break;
@@ -90,25 +98,31 @@ pub fn main() {
                 &port,
                 "midi-input",
                 move |t, ev, _data| {
-                    println!("MIDI event: {} {:?} {}", t, ev, ev.len());
-                    let mut ev_buf = [0u8; 3];
-                    for (i, x) in ev.iter().enumerate() {
-                        ev_buf[i] = *x;
-                    }
-                    let note = Event::Midi(MidiEvent {
-                        data: ev_buf, // [ev[0].to_owned(), ev[1].to_owned(), ev[2].to_owned()],
-                        delta_frames: 0,
-                        live: true,
-                        note_length: None,
-                        note_offset: None,
-                        detune: 0,
-                        note_off_velocity: 0,
-                    });
-                    let events_list = [note];
-                    let mut events_buffer = vst::buffer::SendEventBuffer::new(events_list.len());
-                    events_buffer.store_events(events_list);
-                    let mut plugin = plugin_holder2.lock().unwrap();
-                    plugin.process_events(events_buffer.events());
+                    // println!("MIDI event: {} {:?} {}", t, ev, ev.len());
+                    if ev[0] == 254 { return; }
+                    // let mut ev_buf = [0u8; 3];
+                    // for (i, x) in ev.iter().enumerate() {
+                    //     ev_buf[i] = *x;
+                    // }
+                    // let note = Event::Midi(MidiEvent {
+                    //     data: ev_buf,
+                    //     delta_frames: 0,
+                    //     live: true,
+                    //     note_length: None,
+                    //     note_offset: None,
+                    //     detune: 0,
+                    //     note_off_velocity: 0,
+                    // });
+                    // let events_list = [note];
+                    // let mut events_buffer = vst::buffer::SendEventBuffer::new(events_list.len());
+                    // events_buffer.store_events(events_list);
+                    // let mut plugin = plugin_holder2.lock().unwrap();
+                    // plugin.process_events(events_buffer.events());
+
+                    stream_handle.play_raw(
+                        SineWave::new(1000.0)
+                            .take_duration(Duration::from_millis(100)))
+                        .unwrap()
                 },
                 (),
             ).unwrap();
@@ -223,5 +237,86 @@ impl Sandbox for Ed {
                     .on_press(Message::DecrementPressed),
             )
             .into()
+    }
+}
+
+use cpal::traits::{DeviceTrait, HostTrait};
+use midly::MetaMessage::DeviceName;
+
+
+// TODO To reduce delays try to pick other devices for output
+// (pick a device from cpal and make a rodio stream from it).
+// Experimented with sine wave source and there are similar delays after the midi
+// event as with a plugin-generated sound. So delayz are likely at the output (buffering
+// or the device).
+
+fn list_all_devicess() {
+    // Copy-paste from cpal examples
+
+    println!("Supported hosts:\n  {:?}", cpal::ALL_HOSTS);
+    let available_hosts = cpal::available_hosts();
+    println!("Available hosts:\n  {:?}", available_hosts);
+
+    for host_id in available_hosts {
+        println!("{}", host_id.name());
+        let host = cpal::host_from_id(host_id).unwrap();
+
+
+        let default_in = host.default_input_device().map(|e| e.name().unwrap());
+        let default_out = host.default_output_device().map(|e| e.name().unwrap());
+        println!("  Default Input Device:\n    {:?}", default_in);
+        println!("  Default Output Device:\n    {:?}", default_out);
+
+        let devices = host.devices().unwrap();
+        println!("  Devices: ");
+        for (device_index, device) in devices.enumerate() {
+            println!("  {}. \"{}\"", device_index + 1, device.name().unwrap());
+
+            // Input configs
+            if let Ok(conf) = device.default_input_config() {
+                println!("    Default input stream config:\n      {:?}", conf);
+            }
+            let input_configs = match device.supported_input_configs() {
+                Ok(f) => f.collect(),
+                Err(e) => {
+                    println!("    Error getting supported input configs: {:?}", e);
+                    Vec::new()
+                }
+            };
+            if !input_configs.is_empty() {
+                println!("    All supported input stream configs:");
+                for (config_index, config) in input_configs.into_iter().enumerate() {
+                    println!(
+                        "      {}.{}. {:?}",
+                        device_index + 1,
+                        config_index + 1,
+                        config
+                    );
+                }
+            }
+
+            // Output configs
+            if let Ok(conf) = device.default_output_config() {
+                println!("    Default output stream config:\n      {:?}", conf);
+            }
+            let output_configs = match device.supported_output_configs() {
+                Ok(f) => f.collect(),
+                Err(e) => {
+                    println!("    Error getting supported output configs: {:?}", e);
+                    Vec::new()
+                }
+            };
+            if !output_configs.is_empty() {
+                println!("    All supported output stream configs:");
+                for (config_index, config) in output_configs.into_iter().enumerate() {
+                    println!(
+                        "      {}.{}. {:?}",
+                        device_index + 1,
+                        config_index + 1,
+                        config
+                    );
+                }
+            }
+        }
     }
 }
