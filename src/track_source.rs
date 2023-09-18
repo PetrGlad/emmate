d12736645be77032de6a10032c3d2a05097807d1
Petr Gladkikh <petrglad@gmail.com>
2023-09-18 22:55:55 +0200

    Do not fail when playing beyond the end of the track

--- src/track_source.rs
+++ src/track_source.rs
@@ -19,8 +19,8 @@ impl TrackSource {
         }
     }
 
-    fn note_on_time(&self, i: usize) -> u64 {
-        self.track.events[i].at.as_micros() as u64
+    fn note_on_time(&self, i: usize) -> Option<u64> {
+        self.track.events.get(i).map(|ev| ev.at.as_micros() as u64)
     }
 }
 
@@ -32,15 +32,24 @@ impl EventSource for TrackSource {
     fn seek(&mut self, at: &TransportTime) {
         // Seek back until we cross the `at`, then forward, to stop on the earliest event after
         // the `at` moment. Should work if the target is both before and after the current one.
-        while *at < self.note_on_time(self.current_idx) {
+        while let Some(t) = self.note_on_time(self.current_idx) {
+            if *at >= t {
+                break;
+            }
             self.current_idx -= 1;
             if self.current_idx <= 0 {
                 break;
             }
         }
-        while *at > self.note_on_time(self.current_idx) {
+        if None == self.note_on_time(self.current_idx) {
+            self.current_idx = 0;
+        }
+        while let Some(t) = self.note_on_time(self.current_idx) {
+            if *at <= t {
+                break;
+            }
             self.current_idx += 1;
-            if self.track.events.len() < self.current_idx {
+            if self.track.events.len() <= self.current_idx {
                 break;
             }
         }
@@ -48,7 +57,7 @@ impl EventSource for TrackSource {
     }
 
     fn next(&mut self, at: &TransportTime, queue: &mut BinaryHeap<EngineEvent>) {
-        while self.is_running() {
+        while self.current_idx < self.track.events.len() {
             let notes = &self.track.events;
             let event = &notes[self.current_idx];
             let running_at = event.at.as_micros() as u64;
@@ -76,5 +85,20 @@ impl EventSource for TrackSource {
             }
             self.current_idx += 1;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_lane() {
+        let lane = Arc::new(Box::new(Lane { events: vec![] }));
+        let mut source = TrackSource::new(lane);
+        source.seek(&100_000u64);
+        assert_eq!(source.running_at, 100_000);
+        source.seek(&0);
+        assert_eq!(source.running_at, 0);
     }
 }
